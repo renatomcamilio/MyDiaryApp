5031b43dd45309000c1164cdddc33e2d1e57a90e
Renato Camilio <renatomcamilio@gmail.com>
2015-02-01 21:51:29 -0800

    Initial Commit

---
+++ MyDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDiary
+//
+//  Created by Renato Camilio on 2/1/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
